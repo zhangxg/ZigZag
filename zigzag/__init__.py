a9995561819a08c6698c0aede9094f2dce06d838
zhangxg <dlzhangxg@gmail.com>
2022-12-28 18:09:04 +0800

    using pyximport to run the notebook

--- zigzag/__init__.py
+++ zigzag/__init__.py
@@ -1,3 +1,12 @@
+import pyximport
+import numpy
+
+pyximport.install(
+    setup_args={"include_dirs": numpy.get_include()},
+    language_level=3,
+    reload_support=True,
+)
+
 from zigzag.core import *
 
 PEAK = 1
